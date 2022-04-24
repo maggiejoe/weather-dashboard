61c34a454561e09dd02c037649ee4c82408260e3
maggiejoe <maggiejoseph94@gmail.com>
2022-04-23 20:02:42 -0400

    JavaScript variables added in first function

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,28 @@ var apiKey = fb404b43121bd629a943d011c4a84593;
 var today = moment().format("l");
 var searchList = [];
 
-function currentWeather {
-    var apiUrl = 
+// var citySearchEl = document.querySelector("#cityInput");
+var searchBtnEl = document.querySelector("#searchBtn");
+var todaysWeatherEl = document.querySelector("#todaysWeather");
+
+
+
+function getWeather(city) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=metric";
+
+    fetch(apiUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (searchResponse) {
+        var todaysDate = moment().format("MM/DD/YYYY");
+        var lat = searchResponse.coord.lat;
+        var long = searchResponse.coord.long;
+        var currentWeather = searchResponse.main.temp;
+        var currentWind = searchResponse.wind.speed;
+        var currentHumidity = searchResponse.main.humidity;
+
+        var cityName = document.querySelector("#cityInput");
+    })
 }
+
